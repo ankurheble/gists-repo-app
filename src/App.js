26324f0d51cbc43677fbf00b2ea73a778877a1ea
Ankur Heble <ankurheble91@gmail.com>
2018-08-19 14:44:15 +0530

    fixed routing of repo link

--- src/App.js
+++ src/App.js
@@ -1,22 +1,52 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { MyRepoState } from "./RepoList";
+import { RepoList, Repo } from "./RepoList";
+import { Route } from 'react-router-dom';
 
 class App extends Component {
+  state = {
+    repos: []
+  };
+
+  componentDidMount() {
+    fetch("https://api.github.com/users/ankurheble/repos")
+      .then(response => response.json())
+      .then(response =>
+        this.setState({
+          repos: response
+        })
+      );
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to
-          reload.
-        </p>
-        <MyRepoState />
-      </div>
+      <Fragment>
+        <div className="container">
+          <div className="App">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h1 className="App-title">Welcome to Gists Repo App</h1>
+            </header>
+            <RepoList repos={this.state.repos} />
+            <Route
+              exact
+              path="/repos/:repoName"
+              render={({ match }) => {
+                console.log(match);
+                console.log(this.state.repos);
+                return (
+                  <Repo
+                    repo={this.state.repos.find(
+                      repo => repo.name === match.params.repoName
+                    )}
+                  />
+                );
+              }}
+            />
+          </div>
+        </div>
+      </Fragment>
     );
   }
 }
--- src/RepoList.js
+++ src/RepoList.js
@@ -1,11 +1,11 @@
 import React, { Fragment } from "react";
-import { Link, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export class RepoList extends React.Component {
   render() {
     return (
       <Fragment>
-        <table>
+        <table className="table table-bordered table-sm">
           <thead>
             <tr>
               <th>Repo Name</th>
@@ -30,53 +30,13 @@ export class RepoList extends React.Component {
   }
 }
 
-export class Repo extends React.Component {
-  render() {
-    console.log(this.props);
-
-    return (
-      <div>
-        <h1>{this.props.repo.name}</h1>
-        <p>Owner : {this.props.repo.full_name}</p>
-      </div>
-    );
-  }
-}
-
-export class MyRepoState extends React.Component {
-  state = {
-    repos: []
-  };
-
-  componentDidMount() {
-    fetch("https://api.github.com/users/ankurheble/repos")
-      .then(response => response.json())
-      .then(response =>
-        this.setState({
-          repos: response
-        })
-      );
-  }
-
-  render() {
-    return (
-      <Fragment>
-        <Route
-          exact
-          path="/repos/:repoName"
-          component={({ match }) => {
-            console.log(match);
-            return (
-              <Repo
-                repo={this.state.repos.find(
-                  repo => repo.name === match.params.repoName
-                )}
-              />
-            );
-          }}
-        />
-        <RepoList repos={this.state.repos} />
-      </Fragment>
-    );
-  }
+export function Repo(props) {
+  return (
+    <div>
+      <h1>{props.repo.name}</h1>
+      <p>{props.repo.url}</p>
+      <p>Owner : {props.repo.full_name}</p>
+      <p>{props.repo.clone_url}</p>
+    </div>
+  );
 }
